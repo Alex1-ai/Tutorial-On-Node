bdd58c94fb7b09a10a539abd0c8cd4a43fb79602
alex.ai <alexanderemmanuel1719@gmail.com>
2022-05-02 20:34:37 +0000

    working with node js

---
+++ index.js
@@ -0,0 +1,15 @@
+
+
+//fs means file system
+// const fs = require('fs');
+
+// // copyfileSyn to another file
+// fs.copyFileSync('file.txt',"file2.txt")
+
+var superheroes = require('superheroes');
+var mySuperHero = superheroes.random();
+console.log(mySuperHero)
+
+var supervillains = require('supervillains')
+var mysupervillains = supervillains.random()
+console.log(mysupervillains)
